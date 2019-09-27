a2b1a2f46aef253c634e613f0cc755d188e252a9
Erik Perri <erik.perri@gmail.com>
2019-09-27 18:29:18 -0400

    Added acf requirement checker

---
+++ src/RequirementChecker/AcfChecker.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ryvon\Plugin\RequirementChecker;
+
+class AcfChecker implements RequirementCheckerInterface
+{
+    /**
+     * @var string
+     */
+    private $errorMessage;
+
+    /**
+     * @param string $errorMessage The error message to show to the user if Advanced custom fields is not available.
+     *                             Example: PluginName requires Advanced Custom Fields.
+     */
+    public function __construct($errorMessage)
+    {
+        $this->errorMessage = $errorMessage;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function verify(): array
+    {
+        if (!$this->isAcfActive()) {
+            return [$this->errorMessage];
+        }
+
+        return [];
+    }
+
+    /**
+     * @return bool
+     */
+    private function isAcfActive(): bool
+    {
+        if (!function_exists('is_plugin_active')) {
+            require_once ABSPATH . 'wp-admin/includes/plugin.php';
+        }
+
+        return is_plugin_active('advanced-custom-fields/acf.php')
+            || is_plugin_active('advanced-custom-fields-pro/acf.php');
+    }
+}
